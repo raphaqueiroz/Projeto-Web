e17198d151b2b02d78deb202412855ce69a13fb9
raphaqueiroz <ralphqueiros@gmail.com>
2022-10-12 21:35:52 -0300

    finalizado o javascript

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ let jogadores = [
 
     {
         nome:"Marcos",
-        numero:1 ,
+        numero:"1" ,
         posicao:"goleiro",
+        partidas: 7,
         golmarcados:0 ,
         titular:true // TRUE OU FALSE
     },
     {
         nome:"Cafú",
-        numero: 2,
+        numero: "2",
         posicao:"lateral-direito",
+        partidas: 7,
         golmarcados:0,
         titular:true // TRUE OU FALSE
     },
@@ -19,6 +21,7 @@ let jogadores = [
         nome:"Lúcio",
         numero:"3" ,
         posicao:"zagueiro",
+        partidas: 7,
         golmarcados: 0,
         titular:true // TRUE OU FALSE
     },
@@ -26,6 +29,7 @@ let jogadores = [
         nome:"Roque Júnior",
         numero: "4",
         posicao:"zagueiro",
+        partidas: 6,
         golmarcados:0 ,
         titular:true // TRUE OU FALSE
     },
@@ -33,6 +37,7 @@ let jogadores = [
         nome:"Edmílson",
         numero: "5",
         posicao:"volante e zagueiro",
+        partidas: 6,
         golmarcados:1 ,
         titular:true // TRUE OU FALSE
     },
@@ -40,6 +45,7 @@ let jogadores = [
         nome:"Roberto Carlos",
         numero: "6",
         posicao:"lateral-esquerdo",
+        partidas: 6,
         golmarcados:1 ,
         titular:true // TRUE OU FALSE
     },
@@ -47,6 +53,7 @@ let jogadores = [
         nome:"Ricardinho",
         numero: "7",
         posicao:"meio-campista",
+        partidas: 3,
         golmarcados: 0,
         titular:true // TRUE OU FALSE
     },
@@ -54,6 +61,7 @@ let jogadores = [
         nome:"Gilberto Silva",
         numero: "8",
         posicao:"volante",
+        partidas: 7,
         golmarcados:0 ,
         titular:true // TRUE OU FALSE
     },
@@ -61,6 +69,7 @@ let jogadores = [
         nome:"Ronaldo",
         numero: "9",
         posicao:"atacante",
+        partidas: 7,
         golmarcados:8 ,
         titular:true // TRUE OU FALSE
     },
@@ -68,13 +77,15 @@ let jogadores = [
         nome:"Rivaldo",
         numero: "10",
         posicao:"atacante",
+        partidas: 7,
         golmarcados:5 ,
         titular:true // TRUE OU FALSE
     },
     {
         nome:"Ronaldinho Gaúcho",
         numero: "11",
-        posicao:"meio-camppista",
+        posicao:"meio-campista",
+        partidas: 5,
         golmarcados: 2,
         titular:true // TRUE OU FALSE
     },
@@ -82,18 +93,104 @@ let jogadores = [
         nome:"Dida",
         numero: "12",
         posicao:"goleiro",
+        partidas: 0,
         golmarcados: 0,
-        titular:"false" ,
+        titular: false ,
     },
     {
         nome:"Belletti",
         numero: "13",
         posicao:"lateral-direito",
+        partidas: 1,
         golmarcados: 0,
-        titular:"false" // TRUE OU FALSE
+        titular: false, // TRUE OU FALSE
     }
-    
+    ,
+    {
+        nome:"Rogério Ceni",
+        numero:"22" ,
+        posicao:"goleiro",
+        partidas: 0,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    }
+    ,
+    {
+        nome:"Júnior",
+        numero:"17" ,
+        posicao:"lateral-esquerdo",
+        partidas: 1,
+        golmarcados: 1,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Kleberson",
+        numero:"15" ,
+        posicao:"volante",
+        partidas: 1,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Vampeta",
+        numero:"18" ,
+        posicao:"volante",
+        partidas: 1,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Denílson",
+        numero:"17" ,
+        posicao:"meio-campista",
+        partidas: 5,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Juninho Paulista",
+        numero:"19" ,
+        posicao:"meio-campista",
+        partidas: 5,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Káka",
+        numero:"23" ,
+        posicao:"meio-campista",
+        partidas: 1,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Edílson",
+        numero:"20" ,
+        posicao:"atacante",
+        partidas: 4,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    },
+    {
+        nome:"Luizão",
+        numero:"21" ,
+        posicao:"atacante",
+        partidas: 2,
+        golmarcados: 0,
+        titular: false // TRUE OU FALSE
+    }
 ]
+// remover depois
+jogadores.forEach(jogador =>{
+    jogador.img = "selecao/luizao.jpg"
+})
+console.log(jogadores)
+// Variáveis da semana 11
+const container = document.getElementById('container')
+
+
+
+
 // Semana 5
 function imprimeJogadores(jogadores){
 
@@ -123,8 +220,8 @@ let novoObjeto = {
 
 function imprimeString(objeto) { //IMPRIME UM ÚNICO OBJETO
 
-    console.log(`Este foi um dos jogadores da seleção brasileira de 2002 \n
-                ${objeto.nome}
+    console.log(`
+                Nome: ${objeto.nome}
                 Camisa: ${objeto.numero}
                 Posição? ${objeto.posicao}
                 Marcou ${objeto.golsmarcados} gols.
@@ -146,24 +243,57 @@ imprimeStringObjeto(jogadores)
 // Imprimiir Jogador
 
 
-function pesquisaJogador(jogador, nome){
-    const jogadorRetornado = jogador.filter(player => {
+function pesquisaJogador(){
+    let nome = document.getElementById('pesquisa').value
+    const jogadorRetornado = jogadores.filter(player => {
         return player.nome.toUpperCase().includes(nome.toUpperCase())
     })
-    if (jogadorRetornado.lenth === 0){
-        alert("Jogador não econtrado")
+    console.log(jogadorRetornado)
+    if (jogadorRetornado.length){
+        geraCard(jogadorRetornado)
     } else{
-        return jogadorRetornado
+        alert("Jogador não encontrado")
+        geraCard(jogadores)
     }
 }
 
-let buscaJogador = prompt("Que jogador gostaria de ver?")
+// let buscaJogador = prompt("Que jogador gostaria de ver?")
 
-console.log(pesquisaJogador(jogadores, buscaJogador))
+// console.log(pesquisaJogador(jogadores, buscaJogador))
 
-let jogadorEncontrado = pesquisaJogador(jogadores, buscaJogador)
+// let jogadorEncontrado = pesquisaJogador(jogadores, buscaJogador)
 
-imprimeStringObjeto(jogadorEncontrado)
+// imprimeStringObjeto(jogadorEncontrado)
+
+
+function geraCard (jogadores){
+    let listaJogador = document.getElementById('listaJogador')
+    listaJogador.innerHTML = ''
+    let card 
+    jogadores.map(
+        jogador =>{
+            card = `<section class = "cardJogador" id = "cardJogador">
+            <figure>
+                <img src=${jogador.img} alt="foto ${jogador.nome}">
+            </figure>
+
+            <article>
+                <ul>
+                    <li>Nome: ${jogador.nome}</li>
+                    <li>Camisa: ${jogador.numero}</li>
+                    <li>Posição: ${jogador.posicao}</li>
+                    <li> Marcou:  ${jogador.golmarcados} gols.</li>
+                    <li>Titular: ${jogador.titular ? 'sim' : 'não'}</li>
+                </ul>
+            </article>
+              
+        </section>`
+        listaJogador.innerHTML += card
+        }
+    )
+}
+
+geraCard(jogadores)
 
 
 
